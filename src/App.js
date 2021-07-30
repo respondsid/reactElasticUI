1d854a0a05bd4c0f7ce697dc678d77b6f6d4d9a4
Siddharth Dixit <respondsid@gmail.com>
2021-07-30 02:12:41 -0400

    Streamlined Acccounts sample

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import SearchDashBoard from './components/SearchDashBoard';
 import Layout from './components/Layout/Layout';
-import metadata from './collectionMetadataSampleEmployees.json';
+import metadata from './collectionMetadataSampleAccounts.json';
 function App() {
   return (
     <div className="App">
