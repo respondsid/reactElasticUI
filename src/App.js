4ac186265433be969b1a90640270462aaf3eafb4
Siddharth Dixit <respondsid@gmail.com>
2025-04-17 23:08:20 -0400

    securities

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import SearchDashBoard from "./components/SearchDashBoard";
 import Layout from "./components/Layout/Layout";
-import metadata from "./collectionMetadataSampleEmployees.json";
+import metadata from "./collectionMetadataSampleSecurities.json";
 function App() {
   return (
     <div className="App">
